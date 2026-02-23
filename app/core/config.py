d48a6598a6b62944d3934104d020bc9ffb3f332b
DARKxSHAD0W <ahmedssssahmed9@gmail.com>
2026-02-23 21:09:53 +0300

    Delete app/core/config.py

--- app/core/config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-class Settings:
-    DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
-
-settings = Settings()
